d5952740f4eba684e2ff072286c48103bbb7d30b
ramya <ramyainfo10@gmail.com>
2024-10-16 12:07:44 +0530

    ms2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,20 +12,27 @@ class BinaryTreeNode {
     }
 }
 
-class Tree{
 
-}
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        BinaryTreeNode root = new BinaryTreeNode(arr[i][j+1]);
-        inOrder(arr,root,root,0,0);
+        int[][] arr = {
+                {8, 5},  // 8 is the parent of 5
+                {8, 10}, // 8 is the parent of 10
+                {5, 1},  // 5 is the parent of 1
+                {5, 7},  // 5 is the parent of 7
+                {10, 12} // 10 is the parent of 12
+        };
+
+        BinaryTreeNode root = new BinaryTreeNode(arr[0][1]);
+    BinaryTreeNode res=    inOrder(arr,root,root,0,0);
+        printInOrder(res);
     }
 
 
 
-    public BinaryTreeNode inOrder(int[][] arr,BinaryTreeNode root,BinaryTreeNode parent,int i,int j) {
+    public static BinaryTreeNode inOrder(int[][] arr,BinaryTreeNode root,BinaryTreeNode parent,int i,int j) {
         if(root==null){
             return parent;
         }
@@ -41,6 +48,17 @@ public class Main {
             }
 
 
-        return inOrder(arr,root.left,i+1,j) && inOrder(arr,root.right,i+1.j);
+        inOrder(arr,root.left,parent,i+1,j) ;
+        inOrder(arr,root.right,parent,i+1,j);
+        return parent;
+    }
+
+    public static void printInOrder(BinaryTreeNode root){
+        if(root==null){
+            return ;
+        }
+        printInOrder(root.left);
+        System.out.println(root.data);
+        printInOrder(root.right);
     }
 }
